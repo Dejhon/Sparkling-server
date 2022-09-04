3a1d287c823d2f5fd555d4df831b837708e9ec43
Dejhon <dejhonsherman6@gmail.com>
2022-09-04 11:33:37 -0500

    log

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose');
 require('dotenv').config();
+const mongoose = require('mongoose');
 const app = require('./app');
 // const Service = require('./models/serviceModel');
 
@@ -10,7 +10,7 @@ const DB_CONN = process.env.NODE_ENV === 'production' ?
                 process.env.DATABASE_PRODUCTION.replace('<PWD>', process.env.DATABASE_PASSWORD) :
                 process.env.DATABASE;
 
-
+console.log(DB_CONN);
 mongoose.connect(DB_CONN)
         .then( conn=>{
             console.log("Successfully connected to Sparkling's database");
