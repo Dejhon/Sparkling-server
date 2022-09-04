67d46bb5384377d9ea24a2fcdfa1e538d68a0b5c
Dejhon <dejhonsherman6@gmail.com>
2022-09-04 11:31:26 -0500

    updated env

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+require('dotenv').config();
 const app = require('./app');
 // const Service = require('./models/serviceModel');
 
 
 
-dotenv.config({path: './config.env'})
 
 const DB_CONN = process.env.NODE_ENV === 'production' ?
                 process.env.DATABASE_PRODUCTION.replace('<PWD>', process.env.DATABASE_PASSWORD) :
                 process.env.DATABASE;
-
 
 
 mongoose.connect(DB_CONN)
